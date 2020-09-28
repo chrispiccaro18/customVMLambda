8c8cd5755a22f150cdd2a7ef2f32b11c07416fd3
chrispiccaro <chrispiccaro18@gmail.com>
2020-09-28 13:22:26 -0700

    added prettier

--- index.js
+++ index.js
@@ -1,54 +1,63 @@
-var AWS = require("aws-sdk");
-var nodemailer = require("nodemailer");
-const createEmail = require("./services/createEmail.js");
-const getTranscript = require("./services/getTranscript.js");
+var AWS = require('aws-sdk');
+var nodemailer = require('nodemailer');
+const createEmail = require('./services/createEmail.js');
+const getTranscript = require('./services/getTranscript.js');
 
 var ses = new AWS.SES();
 const transporter = nodemailer.createTransport({
-    SES: ses
+  SES: ses
 });
 
 const s3 = new AWS.S3();
 
 exports.handler = (event, context, callback) => {
-    const { Records } = event;
-    Records.forEach(record => {
-        const newRecord = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
-        const oldRecord = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.OldImage);
-        
-        let transcribeCompleted = (oldRecord.transcribeStatus === "IN_PROGRESS" && newRecord.transcribeStatus === "COMPLETED");
-        if (transcribeCompleted) {
-            getTranscript(`${newRecord.contactId}_${newRecord.timestamp}`)
-            .then(transcript => {
-                const voicemailInfo = { ...newRecord, transcript };
-                createEmail(voicemailInfo, s3)
-                .then(emailContent => {
-                    transporter.sendMail(emailContent, err => {
-                        if (err) {
-                            console.log("Error sending email", err);
-                            callback(err);
-                        } else {
-                            console.log("Email sent successfully");
-                            callback();
-                        }
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                    callback(err);
-                })
+  const { Records } = event;
+  Records.forEach(record => {
+    const newRecord = AWS.DynamoDB.Converter.unmarshall(
+      record.dynamodb.NewImage
+    );
+    const oldRecord = AWS.DynamoDB.Converter.unmarshall(
+      record.dynamodb.OldImage
+    );
+
+    let transcribeCompleted =
+      oldRecord.transcribeStatus === 'IN_PROGRESS' &&
+      newRecord.transcribeStatus === 'COMPLETED';
+    if (transcribeCompleted) {
+      getTranscript(`${newRecord.contactId}_${newRecord.timestamp}`)
+        .then(transcript => {
+          const voicemailInfo = { ...newRecord, transcript };
+          createEmail(voicemailInfo, s3)
+            .then(emailContent => {
+              transporter.sendMail(emailContent, err => {
+                if (err) {
+                  console.log('Error sending email', err);
+                  callback(err);
+                } else {
+                  console.log('Email sent successfully');
+                  callback();
+                }
+              });
             })
             .catch(err => {
-                console.log(err);
-                callback(err);
-            })
-        } else if (newRecord.transcribeStatus === null || newRecord.transcribeStatus === undefined) {
-            // return this._deliver(newVoicemail);
-            console.log('hi');
-        } else {
-            // return Promise.resolve({message: "Unhandled Resolution"});
-            console.log('Transcript Not Ready');
-        }
-    })
-    // callback(null, 'Hello from Lambda');
+              console.log(err);
+              callback(err);
+            });
+        })
+        .catch(err => {
+          console.log(err);
+          callback(err);
+        });
+    } else if (
+      newRecord.transcribeStatus === null ||
+      newRecord.transcribeStatus === undefined
+    ) {
+      // return this._deliver(newVoicemail);
+      console.log('hi');
+    } else {
+      // return Promise.resolve({message: "Unhandled Resolution"});
+      console.log('Transcript Not Ready');
+    }
+  });
+  // callback(null, 'Hello from Lambda');
 };
--- services/createEmail.js
+++ services/createEmail.js
@@ -1,29 +1,31 @@
 const createEmail = (voicemail, s3) => {
-  const expires = 604800
+  const expires = 604800;
   return new Promise((resolve, reject) => {
     const params = {
       Bucket: voicemail.recordingBucketName,
       Key: voicemail.recordingObjectKey,
-      Expires: expires,
+      Expires: expires
     };
     return s3.getSignedUrl('getObject', params, (err, preSignedUrl) => {
-      if(err) {
+      if (err) {
         reject({ error: 'Error getting S3 object url:' + err });
         return;
       }
       const voicemailDate = new Date(voicemail.timestamp * 1000);
-    
+
       let html = `<p>${voicemailDate.toString()}</p>`;
       html += `<p>New voicemail from ${voicemail.contactPhoneNumber}.</p>`;
 
       html += `<b>Voicemail Transcript:</b><p>${voicemail.transcript}</p>`;
-    
-      const expirationDate = new Date(Math.floor((Date.now() / 1000) + expires) * 1000);
+
+      const expirationDate = new Date(
+        Math.floor(Date.now() / 1000 + expires) * 1000
+      );
       html += `<p>Voicemail Expiration Date: ${expirationDate}</p>`;
 
       const audioLink = `<p><a href="${preSignedUrl}">Click Here</a> to listen to the voicemail</p>`;
       html += audioLink;
-    
+
       resolve({
         from: 'uschemwobvm@gmail.com',
         subject: `New voicemail from ${voicemail.contactPhoneNumber}`,
@@ -33,6 +35,6 @@ const createEmail = (voicemail, s3) => {
       });
     });
   });
-}
+};
 
 module.exports = createEmail;
